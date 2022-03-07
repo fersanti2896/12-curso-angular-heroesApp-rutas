84c25738a050cdf50eba39fb281b382f1707ff89
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-06 19:22:25 -0600

    Creación de las rutas hijas de heroes en heroes-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 const routes: Routes = [
+  /* LazyLoad */
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule)
   },
   {
     path: '404',
